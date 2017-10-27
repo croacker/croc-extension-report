2ef7ec9ba9037e011b8460d85b534b7ff21323a8
croacker <7Rv5699400>
2017-10-27 18:53:06 +0800

    work

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,4 +1,9 @@
 /**
+ * Экземпляр контроллера расширения.
+ */
+const extensionInstance = new ExtensionInstance(chrome.runtime.id);
+
+/**
  * Клик на иконке extension, просто сообщаем что клик был.
  */
 chrome.browserAction.onClicked.addListener(function (tab) {
@@ -10,16 +15,16 @@ chrome.browserAction.onClicked.addListener(function (tab) {
  * Подписываемся на обновление страницы.
  */
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        chrome.tabs.executeScript(tabId, {
-            file: 'inject.js'
-        }, 
-        function(){
+    chrome.tabs.executeScript(tabId, {
+        file: 'inject.js'
+    },
+        function () {
             /**
              * Сообщим внедренному скрипту id расширения.
              */
-            chrome.tabs.sendMessage(tabId, {type:'extensionId', id: chrome.runtime.id});;
+            chrome.tabs.sendMessage(tabId, { type: 'extensionId', id: chrome.runtime.id });;
         });
-        console.log('Insert to tab:' + tabId);
+    console.log('Insert to tab:' + tabId);
 });
 
 /**
@@ -28,4 +33,73 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log('background.js: Incoming message');
+        console.log('request ' + request);
+        console.log('sender ' + sender);
+        console.log('sendResponse ' + sendResponse);
     });
+
+/**
+ * Класс контроллера расширения.
+ */
+var ExtensionInstance = function (extensionId) {
+    var me = this;
+    /**
+     * Коллекция закладок.
+     */
+    this.tabs = {};
+
+    /**
+     * Идентификатор расширения.
+     */
+    this.extensionId = extensionId;
+
+    /**
+     * Добавить объект закладки.
+     */
+    this.addTab = function (tab) {
+        me.tabs[tab.id] = tab;
+        tab.extensionId = me.extensionId;
+        me.injectScript(tab);
+    };
+
+    /**
+     * Получить объект закладку.
+     */
+    this.getTab = function (tabId) {
+        return me.tabs[tab.id];
+    };
+
+    this.processTab = function (tabId) {
+        var tab = me.getTab(tabId);
+        if (!tab) {
+            me.addTab(new BrowserTab(tabId));
+        }
+    };
+
+    this.injectScript = function (tab) {
+        chrome.tabs.executeScript(tab.id, {
+            file: 'inject.js'
+        },
+            function () {
+                tab.sendExtensionId();
+            });
+    };
+};
+
+/**
+ * Контроллер закладки.
+ * @param {*} tabId 
+ */
+var BrowserTab = function (tabId) {
+    var me = this;
+    /**
+     * Идентификатор закладки.
+     */
+    this.id = tabId;
+    /**
+         * Сообщим внедренному скрипту id расширения.
+         */
+    this.sendExtensionId = function () {
+        chrome.tabs.sendMessage(tabId, { type: 'extensionId', id: me.extensionId});;
+    };
+}
