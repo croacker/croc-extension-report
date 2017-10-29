bb460e9d0707a5b71fa833d0e6b39bc820f13549
croacker <7Rv5699400>
2017-10-29 14:02:38 +0800

    extension ready

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,30 +1,21 @@
 /**
- * Экземпляр контроллера расширения.
+ * Идентификатор приложения хоста.
  */
-const extensionInstance = new ExtensionInstance(chrome.runtime.id);
+var nativeApplicationName = 'com.croc.native_demo_app';
 
 /**
  * Клик на иконке extension, просто сообщаем что клик был.
  */
 chrome.browserAction.onClicked.addListener(function (tab) {
     console.log('Click Demo Browser Extension');
-    console.log(tab);
+    console.log(extensionInstance.booksInfo);
 });
 
 /**
  * Подписываемся на обновление страницы.
  */
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    chrome.tabs.executeScript(tabId, {
-        file: 'inject.js'
-    },
-        function () {
-            /**
-             * Сообщим внедренному скрипту id расширения.
-             */
-            chrome.tabs.sendMessage(tabId, { type: 'extensionId', id: chrome.runtime.id });;
-        });
-    console.log('Insert to tab:' + tabId);
+    extensionInstance.processTab(tabId, changeInfo, tab);
 });
 
 /**
@@ -32,10 +23,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
  */
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        console.log('background.js: Incoming message');
-        console.log('request ' + request);
-        console.log('sender ' + sender);
-        console.log('sendResponse ' + sendResponse);
+        if(request.type == 'bookInfo'){
+            var bookInfo = request.bookInfo;
+            bookInfo.url = sender.url;
+            extensionInstance.addBookInfo(bookInfo);
+        }
     });
 
 /**
@@ -47,43 +39,78 @@ var ExtensionInstance = function (extensionId) {
      * Коллекция закладок.
      */
     this.tabs = {};
-
+    /**
+     * Коллекция книг полученных со страниц.
+     */
+    this.booksInfo = {};
     /**
      * Идентификатор расширения.
      */
     this.extensionId = extensionId;
 
     /**
-     * Добавить объект закладки.
+     *Обработать объект закладки.
      */
-    this.addTab = function (tab) {
-        me.tabs[tab.id] = tab;
+    this.processTab = function (tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete') {
+            me.addTab(tabId, changeInfo, tab);
+        } else if (changeInfo.status == 'loading') {
+            me.deleteTab(tabId, changeInfo, tab);
+        }
+
         tab.extensionId = me.extensionId;
-        me.injectScript(tab);
+
     };
+
+    /**
+     * Добавить закладку.
+     */
+    this.addTab = function (tabId, changeInfo, tab) {
+        if (me.check(tabId, changeInfo, tab)) {
+            var browserTab = new BrowserTab(tabId);
+            me.tabs[tab.id] = browserTab;
+            me.injectScript(browserTab);
+        };
+    }
+
+    /**
+     * Удалить закладку.
+     */
+    this.deleteTab = function (tabId, changeInfo, tab) {
+        delete me.tabs[tab.id];
+    }
 
     /**
      * Получить объект закладку.
      */
     this.getTab = function (tabId) {
-        return me.tabs[tab.id];
+        return me.tabs[tabId];
     };
 
-    this.processTab = function (tabId) {
-        var tab = me.getTab(tabId);
-        if (!tab) {
-            me.addTab(new BrowserTab(tabId));
-        }
-    };
+    this.addBookInfo = function(bookInfo){
+        var idx = bookInfo.author + '_' + bookInfo.title;
+        me.booksInfo[idx] = bookInfo;
+    }
 
-    this.injectScript = function (tab) {
-        chrome.tabs.executeScript(tab.id, {
+    /**
+     * Внедрить скрипт.
+     */
+    this.injectScript = function (browserTab) {
+        chrome.tabs.executeScript(browserTab.id, {
             file: 'inject.js'
         },
             function () {
-                tab.sendExtensionId();
+                browserTab.sendExtensionId(me.extensionId);
             });
     };
+
+    /**
+     * Условия внедрения скрипта.
+     */
+    this.check = function (tabId, changeInfo, tab) {
+        return !me.getTab(tabId)
+            && tab.url && tab.url.startsWith('https://www.amazon.com/');
+    }
 };
 
 /**
@@ -99,7 +126,67 @@ var BrowserTab = function (tabId) {
     /**
          * Сообщим внедренному скрипту id расширения.
          */
-    this.sendExtensionId = function () {
-        chrome.tabs.sendMessage(tabId, { type: 'extensionId', id: me.extensionId});;
+    this.sendExtensionId = function (extensionId) {
+        chrome.tabs.sendMessage(tabId, { type: 'extensionId', id: extensionId });
     };
+};
+
+/**
+ * Приложение хоста.
+ */
+var NativApplication = function(){
+    var me = this;
+
+    /**
+     * Соединение с приложением хоста.
+     */
+    var port;
+
+    /**
+     * Отправить сообщение приложению хоста.
+     */
+    this.postMessage = function(){
+        var port = me.getPort();
+        if(port){
+            port.postMessage(json);
+        }
+    }
+
+    /**
+     * Получить соединение с приложением хоста.
+     */
+    this.getPort = function(){
+        if (!me.port) {
+            me.port = connectToPort();
+        }
+        return me.port;
+    };
+
+    /**
+     * Подключиться к приложению хоста.
+     */
+    this.connectToPort = function(listener) {
+        me.port = chrome.runtime.connectNative(nativeApplicationName);
+        me.port.onDisconnect.addListener(function (e) {
+            console.log(e);
+            me.port = null;
+        });
+        me.port.onMessage.addListener(function portOnMessageListener(message) {
+            console.log(message);
+        });
+        return port;
+    }
 }
+
+/**
+ * Тип сообщения.
+ */
+const MessageType = {
+    extensionId: 'extensionId',
+    bookInfo: 'bookInfo' 
+};
+
+/**
+ * Экземпляр контроллера расширения.
+ */
+const extensionInstance = new ExtensionInstance(chrome.runtime.id);
--- src/extension/inject.js
+++ src/extension/inject.js
@@ -3,13 +3,64 @@
      * Пишем что внедрились
      */
     console.log('injected to tab');
-
     /**
-     * Подписываемся на события от browser extension.
+     * Подписываемся на события
      */
+    subscribe();
+})();
+
+var browserPage;
+
+/**
+* Подписываемся на события от browser extension.
+*/
+function subscribe() {
     chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
             console.log(request);
-            chrome.runtime.sendMessage(request.id, {msg:'injected'});
+            if(request.type == 'extensionId'){
+                browserPage = new BrowserPage(request.id);
+                browserPage.process();
+            }
+            // chrome.runtime.sendMessage(request.id, { msg: 'injected' });
         });
-})();
+};
+
+/**
+ * Информация о книге.
+ */
+var BookInfo = function(){
+}
+
+/**
+ * 
+ */
+var BrowserPage = function(extensionId){
+    var me = this;
+
+    this.extensionId = extensionId;
+
+    this.process = function(){
+        var bookInfo = me.getBookInfo();
+        if(bookInfo.author && bookInfo.title){
+            me.sendBookInfo(bookInfo);
+        }
+    }
+
+    this.sendBookInfo = function(bookInfo){
+        chrome.runtime.sendMessage(me.extensionId, { type: 'bookInfo', bookInfo: bookInfo });
+    }
+
+    this.getBookInfo = function(){
+        var bookInfo = {};
+        var contributors = document.getElementsByClassName('contributorNameID')
+        if(contributors.length != 0){
+            bookInfo.author = contributors[0].innerHTML;
+        }
+        var productTitle = document.getElementById('productTitle');
+        if(productTitle){
+            bookInfo.title = productTitle.innerHTML;
+        }
+        return bookInfo;
+    }
+}
